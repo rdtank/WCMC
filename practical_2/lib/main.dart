3cde6b059e4f7c4eedf0e48780700db0b270adb0
rdtank <rdtank112@gmail.com>
2020-02-04 22:04:07 +0530

    Practical 2

--- practical_2/lib/main.dart
+++ practical_2/lib/main.dart
@@ -1,30 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SumState();
+  }
+}
+
+class _SumState extends State<MyApp> {
+  double firstnum, secondnum, sum;
   @override
   Widget build(BuildContext context) {
-    var _number1;
     return MaterialApp(
-      title: 'addition',
+      title: 'Practical 2',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Practical 1'),
-          ),
-          body: new Column(
-            Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 0.0),
-                child: TextField(
-                  decoration: InputDecoration(hintText: 'Enter First number'),
-                  controller: _number1,
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-            ),
-            
-          )),
+        appBar: AppBar(
+          title: Text('Practical 2'),
+        ),
+        body: Container(
+            margin: EdgeInsets.all(20.0),
+            child: Center(
+              child: SingleChildScrollView(
+                  child: Column(
+                children: <Widget>[
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration:
+                        InputDecoration(labelText: 'Enter First Number'),
+                    onChanged: (String value) {
+                      setState(() {
+                        firstnum = double.parse(value);
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration:
+                        InputDecoration(labelText: 'Enter Second Number'),
+                    onChanged: (String value) {
+                      setState(() {
+                        secondnum = double.parse(value);
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  RaisedButton(
+                    child: Text('Sum'),
+                    color: Colors.deepOrange,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      sum = firstnum + secondnum;
+                      Fluttertoast.showToast(
+                          msg: 'Sum of two num is $sum',
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIos: 3,
+                          backgroundColor: Colors.deepPurpleAccent,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    },
+                  )
+                ],
+              )),
+            )),
+      ),
     );
   }
 }
