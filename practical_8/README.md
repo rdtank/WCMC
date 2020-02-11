da7ee9be8ad6405523ec2523d1ac8b282a40f683
rdtank <rdtank112@gmail.com>
2020-02-12 00:57:31 +0530

    Practical 8

---
+++ practical_8/README.md
@@ -0,0 +1,6 @@
+# practical_8
+
+Application with the help of a fragment.
+Divide the in two Fragments
+1.  Slide view
+2.  Scroll view
---
+++ practical_8/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carousel',
+      home: MyCarousel(),
+    );
+  }
+}
+
+class MyCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practical 8'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: SingleChildScrollView(child: Column(
+        children: <Widget>[
+          Container(
+            height: 350.0,
+            child: Carousel(
+              images:[
+              ExactAssetImage('assets/1.jpg'),
+              ExactAssetImage('assets/2.jpg'),
+              ExactAssetImage('assets/3.jpg'),
+              ExactAssetImage('assets/4.jpg'),
+              ExactAssetImage('assets/5.jpg'),
+              ],
+              dotSize: 7,
+              dotSpacing: 20.0,
+              showIndicator: true,
+              dotBgColor: Colors.transparent,
+              overlayShadow: true,
+              overlayShadowColors: Colors.white,
+              overlayShadowSize: 0.2,
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          Container(
+              height: 350.0,
+              child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: ((context, index) => ListTile(
+                          title: Text('index $index',style: TextStyle(color: Colors.redAccent,fontSize: 18.0),),
+                          
+                        ))),
+              )
+        ],
+      ),)
+    );
+  }
+}
